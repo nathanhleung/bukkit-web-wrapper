d5a10b76233748592dedea2420c01ff51b4433b1
nathanhleung <nathanhleung@users.noreply.github.com>
2017-11-05 00:40:35 -0400

    fix exports

--- web/db/create-tables.js
+++ web/db/create-tables.js
@@ -10,27 +10,29 @@ const async = require('async');
 const logger = require("../logger");
 const db = require("./db");
 
-const createTablesSqlStatements = fs.readFileSync(
-  path.join(__dirname, "sql", "create-tables.sql"),
-  "utf-8"
-).split("\n\n\n");
+function createTables() {
+  const createTablesSqlStatements = fs.readFileSync(
+    path.join(__dirname, "sql", "create-tables.sql"),
+    "utf-8"
+  ).split("\n\n\n");
 
-async.map(createTablesSqlStatements, createTable, (err, results) => {
-  if (err) {
-    return logger.error("There was an error creating the database tables.");
-  }
-  return logger.debug("All database tables created!");
-});
-
-async function createTable(createTableSql, cb) {
-  db.query(createTableSql, err => {
+  async.map(createTablesSqlStatements, createTable, (err, results) => {
     if (err) {
-      logger.error(err);
-      return cb(err);
+      return logger.error("There was an error creating the database tables.");
     }
-    logger.info("Database table created!");
-    return cb();
+    return logger.debug("All database tables created!");
   });
+
+  function createTable(createTableSql, cb) {
+    db.query(createTableSql, err => {
+      if (err) {
+        logger.error(err);
+        return cb(err);
+      }
+      logger.info("Database table created!");
+      return cb();
+    });
+  }
 }
 
 module.exports = createTables;
