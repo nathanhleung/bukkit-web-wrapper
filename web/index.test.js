465d0bd61fb4fdf1cbaf2aa76779adb6f3d3b548
nathanhleung <nathanhleung@users.noreply.github.com>
2017-11-04 21:42:47 -0400

    setup tests

---
+++ web/index.test.js
@@ -0,0 +1,12 @@
+// Set NODE_ENV to "test" to suppress logging
+process.env.NODE_ENV = "test";
+
+const request = require('supertest');
+
+const app = require('./');
+
+it('responds to GET /', () => {
+  request(app)
+    .get('/')
+    .expect(200, done);
+});
