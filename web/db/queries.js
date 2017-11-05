98d87affcfd0e394965bd35d6fb1cb50a58903e5
nathanhleung <nathanhleung@users.noreply.github.com>
2017-11-05 01:28:52 -0400

    password validation

--- web/db/queries.js
+++ web/db/queries.js
@@ -62,7 +62,16 @@ function addUser(name, email, mc_user, pass, cb) {
   }
 }
 
-function isValidUser(name, email, mc_user, cb) {
+function isValidUser(name, email, mc_user, password, cb) {
+
+  // Check password synchronously
+  if (!validator.isLength(password, { min: 8 })) {
+    cb(null, {
+      valid: false,
+      message: 'Password must be at least 8 characters.',
+    });
+  }
+
   isValidName(name, returnNameStatus);
 
   function returnNameStatus(err, status) {
