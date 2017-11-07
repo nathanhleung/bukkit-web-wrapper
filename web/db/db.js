71aad3d0edd603a1dfece4194768cef35752852a
nathanhleung <nathanhleung@users.noreply.github.com>
2017-11-07 11:11:19 -0500

    use pool instead  of 1 connection

--- web/db/db.js
+++ web/db/db.js
@@ -8,31 +8,17 @@ const logger = require("../logger");
 
 // Specify connection details in
 // .env file
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
-db.connect(err => {
-  if (err) {
+db.on('connection', (connection) => {
+  connection.on('error', (err) => {
     logger.error(err);
-    throw err;
-  } else {
-    logger.info("Sucessfully connected to MySQL Server!");
-  }
-});
-
-db.on("error", err => {
-  logger.error(err);
-  if (err.fatal) {
-    db.connect(err => {
-      if (err) {
-        logger.err(err);
-      }
-    });
-  }
+  });
 });
 
 module.exports = db;
