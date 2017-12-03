4b1425de24bae3af81f01e96f1acc52c186e855c
nathanhleung <nathanhleung@users.noreply.github.com>
2017-12-03 14:10:54 -0500

    revert to original timeframe

--- web/create-restarter.js
+++ web/create-restarter.js
@@ -4,8 +4,7 @@ function createRestarter(minecraftServer) {
   // Stop server every 2 hours
   // This will end the whole process (including web),
   // and then forever will restart.
-  // original: "0 */2 * * *"
-  return scheduleJob("30 * * * * *", () => {
+  return scheduleJob("0 */2 * * *", () => {
     minecraftServer.stdin.write(
       `say To ensure server stability, the server will restart in 5 minutes.\n`
     );
@@ -13,10 +12,10 @@ function createRestarter(minecraftServer) {
       minecraftServer.stdin.write(
         `say To ensure server stability, the server will restart in 1 minute.\n`
       );
-    }, /* 1000 * 60 * 4 */ 10000);
+    }, 1000 * 60 * 4);
     setTimeout(() => {
       minecraftServer.stdin.write(`stop\n`);
-    }, /* 1000 * 60 * 5 */ 5000);
+    }, 1000 * 60 * 5);
   });
 }
 
